52e2a33a2efe9fa257843cec819d6d90e8b1502f
Luis <luiscrs14@gmail.com>
2024-06-08 09:59:28 +0100

    Improve player ball collision

--- entities/ball.py
+++ entities/ball.py
@@ -37,7 +37,7 @@ class Ball(object):
     def manage_brick_collisions(self, bricks: list[Brick]):
         hit = self.ball.collidelist([brick.brick_rect for brick in bricks])
         if hit != -1:
-            hit_location = self._get_hit_location(bricks[hit])
+            hit_location = self._get_brick_hit_location(bricks[hit])
             if hit_location == "VERTICAL":
                 self.speed[1] = -self.speed[1]
             elif hit_location == "HORIZONTAL":
@@ -47,9 +47,13 @@ class Ball(object):
     def manage_player_collisions(self, player: Player):
         # TODO differentiate location of hit
         if self.ball.colliderect(player.player):
-            self.speed[1] = -self.speed[1]
+            hit_location = self._get_player_hit_location(player)
+            if hit_location == "VERTICAL":
+                self.speed[1] = -self.speed[1]
+            elif hit_location == "HORIZONTAL":
+                self.speed[0] = -self.speed[0]
 
-    def _get_hit_location(self, brick: Brick):
+    def _get_brick_hit_location(self, brick: Brick):
         print(f"ball coords {self.ball.x, self.ball.y}")
         print(f"hit {brick} brick. Coords: {brick.x, brick.y}")
         if self.ball.x + self.diameter < brick.x + COLLISION_COMPENSATION:
@@ -64,4 +68,20 @@ class Ball(object):
         if self.ball.y >= brick.y + brick.height - COLLISION_COMPENSATION:
             print("DOWN")
             return "VERTICAL"
+        raise ValueError("No hit location found")
+
+    def _get_player_hit_location(self, player: Player):
+        print(f"ball coords {self.ball.x, self.ball.y}")
+        print(f"hit player. Coords: {player.x, player.y}")
+        if self.ball.x + self.diameter < player.x + COLLISION_COMPENSATION:
+            print("LEFT")
+            return "HORIZONTAL"
+        if self.ball.x > player.x + player.width - COLLISION_COMPENSATION:
+            print("RIGHT")
+            return "HORIZONTAL"
+        if self.ball.y + self.diameter < player.y + COLLISION_COMPENSATION:
+            print("UP")
+            return "VERTICAL"
+        if self.ball.y >= player.y + player.height - COLLISION_COMPENSATION:
+            raise ValueError("Hits should never happen downwards!")
         raise ValueError("No hit location found")
